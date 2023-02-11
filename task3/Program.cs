5505fa36708a7359992ee7317808e2a092a07526
Sergei <sergeimamedov9976@gmail.com>
2023-02-12 03:12:22 +0600

    третья задача

---
+++ task3/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] GenerateArray(int row, int col)
+{
+    int[,] array = new int[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return array;
+}
+void PrintArray(int[,] array)
+{
+    int row = array.GetLength(0);
+    int col = array.GetLength(1);
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+int Average(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        int sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum = sum + array[i, j];
+        }
+        return sum;
+    }
+}
+int[,] array = GenerateArray(3, 4);
+PrintArray(array);
+int sum = Average(array);
+Console.Write($"{sum / array.GetLength(0)} ");
+
+ //ИЗВИНИТЕ, НЕ ПОНИМАЮ ЧТО ЕЩЕ НУЖНО ВЕРНУТЬ, В ТЕРМИНАЛЕ ВЫДАЕТ ОШИБКУ
